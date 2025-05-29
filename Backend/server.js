c56f436cd4451e5059e930e4b39d5ff3211335ec
Pranjal6955 <negipranjal555@gmail.com>
2025-05-30 04:21:12 +0530

    Fixes 404

--- Backend/server.js
+++ Backend/server.js
@@ -107,7 +107,10 @@ cleanupInvalidReminders().then(() => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://frontend-sprouty.vercel.app/', 'http://localhost:5173'],
+  credentials: true
+}));
 
 // Body parser with increased limit for image uploads
 app.use(express.json({ limit: '10mb' }));
